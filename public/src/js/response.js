ff130c340b8f1719e0c817bc93d64f660fb3d3a9
shoolop <51280228+shoolop@users.noreply.github.com>
2020-02-19 17:55:46 +0800

    new web

--- public/src/js/response.js
+++ public/src/js/response.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('https://shoolop.github.io/practice/public/service-worker.js').then(function() {
+        navigator.serviceWorker.register('practice/public/service-worker.js').then(function() {
             console.log('Service Worker 註冊成功');
         }).catch(function(error) {
             console.log('Service worker 註冊失敗:', error);
