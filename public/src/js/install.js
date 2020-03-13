ad6ecf805a816a93209b778c98ca75d040ce448f
shoolop <51280228+shoolop@users.noreply.github.com>
2020-03-13 11:00:20 +0800

    update page

--- public/src/js/install.js
+++ public/src/js/install.js
@@ -21,3 +21,9 @@ function InstallModal() {
         deferredPrompt = null;
     }
 }
+
+function InstallAlarm() {
+    if (confirm("確定要下載鬧鐘chrome插件嗎?")) {
+        location.href = 'https://drive.google.com/file/d/107XQeF5XJRfkzXbJOky9iiiWqNXrZ_wc/view?usp=sharing';
+    }
+}
