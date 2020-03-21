726e1890252f962b8db8a09f6874e26148a03f89
shoolop <51280228+shoolop@users.noreply.github.com>
2020-03-21 14:08:53 +0800

    add download

---
+++ public/src/js/Alerm.js
@@ -0,0 +1,6 @@
+
+function InstallAlarm() {
+    if (confirm("確定要下載鬧鐘chrome插件嗎?")) {
+        location.href = 'https://drive.google.com/file/d/107XQeF5XJRfkzXbJOky9iiiWqNXrZ_wc/view?usp=sharing';
+    }
+}
--- public/src/js/install.js
+++ public/src/js/install.js
@@ -21,9 +21,3 @@ function InstallModal() {
         deferredPrompt = null;
     }
 }
-
-function InstallAlarm() {
-    if (confirm("確定要下載鬧鐘chrome插件嗎?")) {
-        location.href = 'https://drive.google.com/file/d/107XQeF5XJRfkzXbJOky9iiiWqNXrZ_wc/view?usp=sharing';
-    }
-}
